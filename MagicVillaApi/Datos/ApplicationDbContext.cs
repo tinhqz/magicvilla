388836c06c9c16b4f3a641de1a29d03fa165f381
Agustin H <henriquezagustin@hotmail.com>
2023-07-25 21:32:54 -0600

    ejecutando migracion inicial

--- MagicVillaApi/Datos/ApplicationDbContext.cs
+++ MagicVillaApi/Datos/ApplicationDbContext.cs
@@ -3,8 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MagicVillaApi.Datos
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<Villa> Villas { get; set; }
+
+        // comandos para migraciones
+        // $ add-migration agregarBaseDatos
+        // $ update-database
     }
 }
--- MagicVillaApi/Program.cs
+++ MagicVillaApi/Program.cs
@@ -1,3 +1,6 @@
+using MagicVillaApi.Datos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// agregando cadena de coneccion
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
